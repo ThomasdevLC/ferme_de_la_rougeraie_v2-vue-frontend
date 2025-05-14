cc92500116689ff7ae458a214af6a91211d6cec4
ThomasdevLC <thomas.lecam@gmail.com>
2025-05-14 12:19:14 +0200

    tailwind v4 config

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,28 +1,41 @@
 <template>
-  <div v-if="loading" class="text-center py-8 text-gray-500">Chargement des produits...</div>
+  <div
+    v-if="loading"
+    class="text-center py-8 text-gray-3 font-base"
+  >
+    Chargement des produits...
+  </div>
 
-  <div v-else class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
+  <div
+    v-else
+    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4"
+  >
     <div
       v-for="product in products"
       :key="product.id"
-      class="bg-white border border-gray-200 rounded-2xl p-4 text-center"
+      class="bg-gray-1 border border-gray-2 font-base rounded-2xl p-4 text-center shadow-sm"
     >
       <img
         :src="`http://localhost:8000${product.image}`"
         :alt="`Image de ${product.name}`"
         class="w-full h-40 object-cover rounded-lg mb-4"
       />
-      <h3 class="text-lg font-semibold text-gray-800">{{ product.name }}</h3>
-      <p class="text-gray-600 mb-1">{{ product.price }} € / {{ product.unit }}</p>
+      <h3 class="text-lg font-semibold text-primary font-titles">
+        {{ product.name }}
+      </h3>
+      <p class="text-gray-4 font-base mb-1">
+        {{ product.price }} € / {{ product.unit }}
+      </p>
       <p
         v-if="product.discount"
-        class="text-red-600 font-bold text-sm mt-1"
+        class="text-secondary font-bold text-sm mt-1"
       >
         🔥 Promo disponible
       </p>
     </div>
   </div>
 </template>
+
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
