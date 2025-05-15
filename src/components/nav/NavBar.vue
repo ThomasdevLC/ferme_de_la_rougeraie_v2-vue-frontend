7577f26d897c130bb8c8f8900780a31268fd0141
ThomasdevLC <thomas.lecam@gmail.com>
2025-05-15 16:56:31 +0200

    navbar+ new router

---
+++ src/components/nav/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="bg-white border-b border-gray-200 shadow-sm px-6 py-4">
+    <div class="flex justify-between items-center max-w-6xl mx-auto">
+      <!-- Logo ou titre -->
+      <h1 class="text-xl font-bold text-primary">La Rougeraie</h1>
+
+      <!-- Liens de navigation -->
+      <ul class="flex gap-6 text-sm font-medium text-gray-700">
+        <li>
+          <RouterLink
+            to="/products"
+            class="hover:text-primary transition"
+            active-class="text-primary"
+          >
+            Produits
+          </RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/about" class="hover:text-primary transition" active-class="text-primary">
+            À propos
+          </RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/login" class="hover:text-primary transition" active-class="text-primary">
+            Connexion
+          </RouterLink>
+        </li>
+
+        <li>
+          <p class="cursor-pointer hover:text-primary transition" @click="ui.openCart">panier</p>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import { useUIStore } from '@/stores/uiStore'
+
+const ui = useUIStore()
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import ProductView from '@/views/ProductView.vue'
-import CartView from '@/views/CartView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,12 +12,6 @@ const router = createRouter({
     },
 
     {
-      path: '/cart',
-      name: 'cart',
-      component: CartView,
-    },
-
-    {
       path: '/about',
       name: 'about',
       // route level code-splitting
@@ -27,6 +19,12 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LoginView.vue'),
+    }
   ],
 })
 
