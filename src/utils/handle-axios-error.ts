e723c4b5cfc2c6862618f8439fbe719382d386a0
ThomasdevLC <thomas.lecam@gmail.com>
2025-05-28 14:58:32 +0200

    handle axios errors multiple errors case

--- src/utils/handle-axios-error.ts
+++ src/utils/handle-axios-error.ts
@@ -2,10 +2,25 @@ import axios from 'axios';
 
 export function handleAxiosError(error: unknown): string {
   if (axios.isAxiosError(error)) {
-    return error.response?.data?.error ?? 'Erreur du serveur.';
+    const data = error.response?.data;
+
+    if (typeof data?.error === 'string') {
+      return data.error;
+    }
+
+    if (data?.errors && typeof data.errors === 'object') {
+      return Object
+        .values(data.errors)
+        .flat()
+        .join('\n');
+    }
+
+    return 'Erreur du serveur.';
   }
+
   if (error instanceof Error) {
     return error.message;
   }
+
   return 'Erreur réseau ou serveur inconnue.';
 }
