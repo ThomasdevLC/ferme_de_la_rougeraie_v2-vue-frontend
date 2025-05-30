364103833102b66ff413c04be6351a5788674e3d
ThomasdevLC <thomas.lecam@gmail.com>
2025-05-30 10:34:46 +0200

    navbar conditional render

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
-import { onMounted } from 'vue'
+import { RouterView, useRouter } from 'vue-router'
+import { onMounted, computed } from 'vue'
 import CartModal from '@/components/cart/CartModal.vue'
 import Navbar from '@/components/nav/NavBar.vue'
 import { useUIStore } from '@/stores/ui-store'
@@ -10,6 +10,11 @@ import { useAuthStore } from '@/stores/auth-store'
 const ui = useUIStore()
 const user = useUserStore()
 const auth = useAuthStore()
+
+const router = useRouter()
+const route = router.currentRoute
+
+const displayNavbar = computed(() => route.value.path !== '/login')
 
 async function initSession() {
   if (!auth.isTokenValid) {
@@ -26,24 +31,20 @@ async function initSession() {
   }
 }
 
-
 onMounted(() => {
   initSession()
 })
-
-
 </script>
 
 <template>
-  <header class=" fixed z-50 w-full">
+  <header v-if="displayNavbar" class="fixed z-50 w-full">
     <Navbar />
   </header>
   <main @click="ui.closeUserMenu" class="relative min-h-screen">
     <img src="/assets/background.png"
          alt="Background"
-         class="absolute inset-0 w-full h-full object-cover pointer-events-none z-0" />
+         class="fixed inset-0 w-full h-full object-cover pointer-events-none z-0" />
     <RouterView />
     <CartModal class="z-60" />
   </main>
-
 </template>
