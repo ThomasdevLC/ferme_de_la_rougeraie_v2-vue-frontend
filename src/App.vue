48240bcb91631e3d0d51c8660c4ca036c7618511
ThomasdevLC <thomas.lecam@gmail.com>
2025-05-31 15:54:20 +0200

    message api call

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,24 @@ import { RouterView, useRouter } from 'vue-router'
 import { onMounted, computed } from 'vue'
 import CartModal from '@/components/cart/CartModal.vue'
 import Navbar from '@/components/nav/NavBar.vue'
+import MessageInfosMarquee from '@/components/message/MessageInfosMarquee.vue'
 import { useUIStore } from '@/stores/ui-store'
 import { useUserStore } from '@/stores/user-store'
 import { useAuthStore } from '@/stores/auth-store'
+import { useMessageStore } from '@/stores/message-store'
+
 
 const ui = useUIStore()
 const user = useUserStore()
 const auth = useAuthStore()
+const messageStore = useMessageStore()
+
 
 const router = useRouter()
 const route = router.currentRoute
 
 const displayNavbar = computed(() => route.value.path !== '/login')
+
 
 async function initSession() {
   if (!auth.isTokenValid) {
@@ -33,6 +39,8 @@ async function initSession() {
 
 onMounted(() => {
   initSession()
+  messageStore.loadMessages()
+
 })
 </script>
 
@@ -40,6 +48,8 @@ onMounted(() => {
   <header v-if="displayNavbar" class="fixed z-50 w-full">
     <Navbar />
   </header>
+
+
   <main @click="ui.closeUserMenu" class="relative min-h-screen text-text-color pt-64 pb-12">
     <img src="/assets/background.png"
          alt="Background"
