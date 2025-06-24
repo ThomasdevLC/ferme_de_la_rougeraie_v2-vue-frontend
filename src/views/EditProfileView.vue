658d856388d5133ddfe6b16898260f53f2542f57
ThomasdevLC <thomas.lecam@gmail.com>
2025-06-24 14:34:27 +0200

    Refactors profile editing and adds account deletion
    Moves user profile data management to the user store for improved data consistency.
    
    Adds functionality to delete user accounts, including confirmation and redirection after deletion.
    
    Improves error handling and user feedback for profile updates and account deletion.

--- src/views/EditProfileView.vue
+++ src/views/EditProfileView.vue
@@ -8,18 +8,18 @@
           <div class="flex gap-2 items-center mb-2 md:mb-0">
             <IdCard class="w-12 h-12" :stroke-width="1" />
             <p class="font-bold text-2xl">
-              <span> {{ user.profile?.firstName }}&nbsp;</span
-              ><span>{{ user.profile?.lastName }}</span>
+              <span> {{ userStore.profile?.firstName }}&nbsp;</span
+              ><span>{{ userStore.profile?.lastName }}</span>
             </p>
           </div>
           <div class="flex flex-col md:flex-row gap-4">
             <div class="flex gap-2 items-center">
               <Mail class="w-7 h-7" :stroke-width="1" />
-              <p class="text-xl">{{ user.profile?.email }}</p>
+              <p class="text-xl">{{ userStore.profile?.email }}</p>
             </div>
             <div class="flex gap-2 items-center">
               <Phone class="w-7 h-7" :stroke-width="1" />
-              <p class="text-xl">{{ user.profile?.phone }}</p>
+              <p class="text-xl">{{ userStore.profile?.phone }}</p>
             </div>
           </div>
         </div>
@@ -54,7 +54,8 @@
           </button>
 
           <button
-            @click="user.logout()"
+            type="button"
+            @click="onDeleteAccount"
             class="w-full md:w-fit bg-red-500 text-white py-2 px-4 rounded-sm hover:bg-opacity-90 cursor-pointer flex items-center justify-center gap-2"
           >
             <UserRoundX class="w-5 h-5" :stroke-width="1" />
@@ -127,9 +128,9 @@
           </button>
         </div>
 
-        <p v-if="user.error" class="mb-4 p-3 bg-red-100 text-red-800 text-center rounded">
+        <p v-if="error" class="mb-4 p-3 bg-red-100 text-red-800 text-center rounded">
           <CircleX class="inline-block mr-2" :stroke-width="1" />
-          {{ user.error }}
+          {{ error }}
         </p>
         <p v-else-if="successMessage" class="mb-4 p-3 bg-green-100 text-green-800 text-center rounded">
           <Check class="inline-block mr-2" :stroke-width="1" />
@@ -140,21 +141,32 @@
   </div>
 </template>
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 import { useForm, useField } from 'vee-validate'
 import * as yup from 'yup'
-import { useUserStore } from '@/stores/user-store'
-import { updateUserProfile } from '@/services/user/user-profile-service.ts'
 import type { UserProfileUpdate } from '@/models/user/user-profile-update'
+import { useUserStore } from '@/stores/user-store'
 import { handleAxiosError } from '@/utils/handle-axios-error'
 import { handleAxiosSuccess } from '@/utils/handle-axios-success'
 import { IdCard, Mail, Phone, Pencil, PencilOff, UserRoundX , CircleX, Check  } from 'lucide-vue-next'
 
-const user = useUserStore()
+const userStore = useUserStore()
+const router = useRouter()
 
 const updatePhone = ref(false)
 const updatePassword = ref(false)
 const successMessage = ref('')
+const error = ref<string>('')
+
+onMounted(async () => {
+  if (!userStore.profile) {
+     {
+      await userStore.loadProfile()
+    }
+  }
+})
+
 
 const schema = computed(() =>
   yup.object({
@@ -235,24 +247,43 @@ const onSubmit = handleSubmit(async (values) => {
   }
 
   try {
-    const response = await updateUserProfile(payload)
+    const response = await userStore.updateUserProfile(payload)
     successMessage.value = handleAxiosSuccess(response)
     updatePhone.value = false
     updatePassword.value = false
-    await user.loadProfile()
+    await userStore.loadProfile()
     resetForm()
     setTimeout(() => {
       successMessage.value = ''
     }, 3000)
   } catch (err) {
-    user.error = handleAxiosError(err)
+    error.value = handleAxiosError(err)
     setTimeout(() => {
-      user.error = ''
+      error.value = ''
       updatePhone.value = false
       updatePassword.value = false
       resetForm()
-    }, 3000)
+    }, 5000)
 
   }
 })
+
+async function onDeleteAccount() {
+  try {
+    const { data } = await userStore.deleteUserAccount()
+    successMessage.value = data.message
+    setTimeout(() => {
+      successMessage.value = ''
+      router.push('/login')
+      userStore.logout()
+    }, 3000)
+
+  } catch (err: unknown) {
+    error.value = handleAxiosError(err)
+    setTimeout(() => { error.value = '' }, 3000)
+  }
+}
+
+
+
 </script>
