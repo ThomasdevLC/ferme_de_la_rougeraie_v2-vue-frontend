7fd54f367d0b6e95257d67d24890bb8be6042ba0
ThomasdevLC <thomas.lecam@gmail.com>
2026-02-22 15:53:57 +0100

    add availableStock in cart items

--- src/components/order/OrderCard.vue
+++ src/components/order/OrderCard.vue
@@ -84,8 +84,8 @@ const fetchOrder = async () => {
       orderData.pickupDate.slice(0, 10)
     )
 
-    orderData.items.forEach(item => {
-      cart.addToCart(item.product, item.quantity)
+    orderData.items.forEach((item: any) => {
+      cart.addToCart(item.product, item.quantity, item.availableStock ?? null)
     })
     ui.openCart()
   } catch (error) {
